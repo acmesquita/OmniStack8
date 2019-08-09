2f43a026d8007863363f2ce2445dac035fff80fb
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-08-09 15:50:04 -0300

    projeto tindev - semana omnistack

---
+++ backend/src/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectUsers = {};
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query
+  connectUsers[user] = socket.id;
+});
+
+mongoose.connect('mongodb+srv://catharina:catharina@cluster0-j6zdt.mongodb.net/omnistack8?retryWrites=true&w=majority',{
+  useNewUrlParser: true 
+});
+
+app.use((req, res, next)=>{
+  req.io = io;
+  req.connectUsers = connectUsers;
+
+  return next();
+});
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+server.listen(3333);
